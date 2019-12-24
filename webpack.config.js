78e30d4e2877471b2f9f9f3345869cc11dd3a6ee
o_O <musixnotmusic@gmail.com>
2019-12-24 18:04:39 +0800

    Update webpack.config.js
    webpack.base can loader css

--- webpack.config.js
+++ webpack.config.js
@@ -44,6 +44,21 @@ module.exports = {
           "ts-loader"
         ], 
         exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1,
+              sourceMap: true,
+            },
+          },
+        ],
       }
     ]
   }
